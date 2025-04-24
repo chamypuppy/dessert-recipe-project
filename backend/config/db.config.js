87b9c0a5b570952483551d2f7b08c1fc6211e36e
chamypuppy <irisrebelle@gmail.com>
2025-04-24 18:30:19 +0900

    db.config와 root env에 db_port 추가

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -4,7 +4,8 @@ const dbConfig = {
   host: process.env.DB_HOST,       // 환경 변수에서 DB_HOST 가져오기
   user: process.env.DB_USER,       // 환경 변수에서 DB_USER 가져오기
   password: process.env.DB_PASSWORD, // 환경 변수에서 DB_PASSWORD 가져오기
-  database: process.env.DB_NAME    // 환경 변수에서 DB_NAME 가져오기
+  database: process.env.DB_NAME,    // 환경 변수에서 DB_NAME 가져오기
+  port: process.env.DB_PORT        // 클타 MySQL 포트!
 };
 
 module.exports = dbConfig;
